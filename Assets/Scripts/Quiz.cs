5c3f947e0f72c0ee4bc6b8c4af698c8271d87aed
Blair-Ciolk-Student <112421414+Blair-Ciolk-Student@users.noreply.github.com>
2023-03-23 14:30:44 -0400

    Commit 2

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSrite;
+    [SerializeField] Sprite correctAnswerSprite;
+
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+    }
+}
